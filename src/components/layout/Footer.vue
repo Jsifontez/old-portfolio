cca04f5efccd346155b3b686cf70f7d882e1b70c
Juan Sifontez <juansfontz@gmail.com>
2018-01-20 03:19:29 -0400

    Footer view changed

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,43 +1,21 @@
 <template lang="html">
   <footer id="contact">
     <b-row>
-      <b-col cols="12" class="text-center">
-        <h4>
-          <a href="mailto:juansfontz@gmail.com">
-            <strong>¡Contrátame! 🚀 </strong>
-          </a>
-        </h4>
-        <p> Si prefieres puedes contactarme por mis otras redes sociales 😉</p>
-      </b-col>
-
-      <b-col cols="8" offset="2">
-        <b-row>
-          <b-col cols="3">
-            <h4>
-              <a href="https://twitter.com/Jsifontz_?lang=es">Twitter</a>
-            </h4>
-          </b-col>
-          <b-col cols="3">
-            <h4>
-              <a href="https://github.com/JSifontz">Github</a>
-            </h4>
-          </b-col>
-          <b-col cols="3">
-            <h4>
-              <a href="https://www.linkedin.com/in/juan-sifontez-924761133/">LinkedIn</a>
-            </h4>
-          </b-col>
-          <b-col cols="3">
-            <h4>
-              <a href="https://www.facebook.com/juansifontez/">Facebook</a>
-            </h4>
-          </b-col>
-        </b-row>
-      </b-col>
-
       <b-col cols="12 text-center">
         <p>
-        Juan Sifontez © 2018
+          Juan Sifontez © 2018
+          <a href="mailto:juansfontz@gmail.com">
+            <strong><i class="far fa-envelope"></i></strong>
+          </a>
+          <a href="https://twitter.com/Jsifontz_?lang=es" target="_blank">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="https://github.com/JSifontz" target="_blank">
+            <i class="fab fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/juan-sifontez-924761133/" target="_blank">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
         </p>
       </b-col>
 
