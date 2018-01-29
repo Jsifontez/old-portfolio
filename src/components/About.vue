6f67e07c9124f814da7c10d53f8a4486d5dcd8f4
Juan Sifontez <juansfontz@gmail.com>
2018-01-29 16:53:30 -0400

    some drafting changes

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,13 +6,13 @@
       </b-col>
       <b-col md="6" offset-md="3" class="description">
         <p>
-          I'm a <strong>self-taught</strong> person that enjoy <strong>learn new things</strong> and apply that knowledge in real world projects. Determined to <strong>achieve goals</strong>.
+          I'm a <strong>self-taught</strong> person that enjoy <strong>learning new things</strong> and apply that knowledge in real world projects. Determined to <strong>achieve goals</strong>.
         </p>
         <p>
-          I'm a Junior <strong>frontend developer</strong> with knowledge of HTML, CSS and Javascript, capable of handling <strong>VueJs</strong>, <strong>Reacts</strong>, <strong>Materialize</strong>, <strong>Bootstrap</strong> technologies.
+          I'm a Junior <strong>frontend developer</strong> with knowledge of HTML, CSS and Javascript, capable of handling <strong>VueJs</strong>, <strong>Reacts</strong>, <strong>Materialize</strong>, <strong>Bootstrap</strong> based technologies.
         </p>
         <p>
-          Currently I'm developing <strong>personal projects</strong> to fortify all my knowledges. In the future I'm want to have a job as a <strong>frontend</strong>. Besides of this I'm interesting in be part of areas such as <strong>machine learning</strong> and <strong>VR/AR</strong>.
+          Currently I'm developing <strong>personal projects</strong> to make strong all of my knowledge. In the future I'm want a job as a <strong>frontend</strong>. Besides of this I'm interested in being part of areas such as <strong>machine learning</strong> and <strong>VR/AR</strong>.
         </p>
       </b-col>
       <b-col md="12" class="text-center">
